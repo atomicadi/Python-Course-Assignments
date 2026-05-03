3f10b7d7e45bec4cc2b1670c330dfd5465fbc8af
Aditya Barman <147025377+atomicadi@users.noreply.github.com>
2026-05-03 13:53:30 +0300

    Calc_q_trans_library

--- Day03/Calc_q_trans_library.py
+++ Day03/Calc_q_trans_library.py
@@ -8,20 +8,21 @@ def calc_q_trans(m, T, V):
     h = constants.h
     k_B = constants.k
 
-if not m.isdigit():
-   return None, "Oops! you have given either wrong mass or negative mass!"
-else:
-   m = int(m)
+    if not m.isdigit():
+        return None, "Oops! you have given either wrong mass or negative mass!"
+    else:
+        m = int(m)
 
-if not T.isdigit():
-   return None, "Oops! you have given either wrong temperature or negative temperature!"
-else:
-   T = int(T)
+    if not T.isdigit():
+        return None, "Oops! you have given either wrong temperature or negative temperature!"
+    else:
+        T = int(T)
 
-if not V.isdigit():
-   return None, "Oops! you have given either wrong volume or negative volume!"
-else:
-   V = int(V)
+    if not V.isdigit():
+        return None, "Oops! you have given either wrong volume or negative volume!"
+    else:
+        V = int(V)
 
-q_trans = ((2*(math.pi)*m*k_B*T)/h**2)**(3/2) * V
-return q_trans, None
+    q_trans = ((2 * math.pi * m * k_B * T) / h**2)**(3/2) * V
+
+    return q_trans, None
