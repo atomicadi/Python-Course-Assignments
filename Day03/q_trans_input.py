bb7fae5610f795250cab487ebc376e9fe205207d
Aditya Barman <147025377+atomicadi@users.noreply.github.com>
2026-05-03 11:36:42 +0300

    q_trans calculation
    Implement q_trans_input script to calculate q_trans using user inputs for mass, temperature, and volume.

---
+++ Day03/q_trans_input.py
@@ -0,0 +1,15 @@
+from Calc_q_trans_library import calc_q_trans
+
+m = input("put the value of mass in kg unit")
+T = input("put the value of temperature in K unit")
+V = input("put the value of volume in cm3 unit)
+
+q_trans, error = calc_q_trans(m, T, V)
+
+if error:
+          print(error)
+else:
+    print(
+          
+
+          
