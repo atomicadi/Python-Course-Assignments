851163d59e1c03c9b785f9af26ab2863db365017
Aditya Barman <147025377+atomicadi@users.noreply.github.com>
2026-05-04 06:43:37 +0300

    Fix comment syntax in q_trans_input.py

--- Day03/q_trans_input.py
+++ Day03/q_trans_input.py
@@ -1,5 +1,5 @@
-! Writtenn by Aditya Barman; May 03, 2026 @weizmann
-! calculating translational partition function using user input
+# Writtenn by Aditya Barman; May 03, 2026 @weizmann
+# calculating translational partition function using user input
 
 from Calc_q_trans_library import calc_q_trans
 
