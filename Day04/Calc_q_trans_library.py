378bf67e0f325574860d1d2c6b9ebb12d0fb6e06
Aditya Barman <147025377+atomicadi@users.noreply.github.com>
2026-05-09 23:37:40 +0300

    Modify calc_q_trans function for temperature conversion
    Updated temperature conversion logic and parameter handling.

--- Day04/Calc_q_trans_library.py
+++ Day04/Calc_q_trans_library.py
@@ -1,7 +1,7 @@
-# copied from Day02 and edited by Aditya Barman, Date: May 03, 2026; @weizmann
+# copied from Day03 and edited by Aditya Barman, Date: May 09, 2026; @weizmann
 # calculating translational partition function
 
-def calc_q_trans(m, T, V):
+def calc_q_trans(m, TC, T, V):
     import math
     from scipy import constants
 
@@ -12,6 +12,11 @@ def calc_q_trans(m, T, V):
         return None, "Oops! you have given either wrong mass or negative mass!"
     else:
         m = int(m)
+    
+    if not TC.isdigit():
+        return None, "Oops! you have given either wrong temperature checker or negative temperature checker!"
+    else:
+        TC = int(TC)
 
     if not T.isdigit():
         return None, "Oops! you have given either wrong temperature or negative temperature!"
@@ -22,6 +27,13 @@ def calc_q_trans(m, T, V):
         return None, "Oops! you have given either wrong volume or negative volume!"
     else:
         V = int(V)
+
+    if TC = 0:
+        T = T + 273.15
+    elif TC = 1:
+        T = (T + 459.67)/1.8
+    else:
+        T = T
 
     q_trans = ((2 * math.pi * m * k_B * T) / h**2)**(3/2) * V
 
