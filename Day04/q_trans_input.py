d1033c9d7cfbc4707125bb36d44eefb05b16fd14
Aditya Barman <147025377+atomicadi@users.noreply.github.com>
2026-05-09 23:40:58 +0300

    Update temperature input handling in q_trans_input.py

--- Day04/q_trans_input.py
+++ Day04/q_trans_input.py
@@ -1,13 +1,14 @@
-# Writtenn by Aditya Barman; May 03, 2026 @weizmann
+# Copied from Day03 and edited by Aditya Barman; May 09, 2026 @weizmann
 # calculating translational partition function using user input
 
 from Calc_q_trans_library import calc_q_trans
 
 m = input("put the value of mass in kg unit: ")
+TC = input("Temperature checker, if in Degree Celsius, put 0, if in Fahrenheit, put 1, and if in Kelvin, put 3: ")
 T = input("put the value of temperature in K unit: ")
 V = input("put the value of volume in m3 unit (if there is no V, put 1): ")
 
-q_trans, error = calc_q_trans(m, T, V)
+q_trans, error = calc_q_trans(m, TC, T, V)
 
 if error:
           print(error)
