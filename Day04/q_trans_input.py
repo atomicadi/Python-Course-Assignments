2fe15b0e6be780c4a6281244e2c003e483926df7
Aditya Barman <147025377+atomicadi@users.noreply.github.com>
2026-05-09 23:44:33 +0300

    Modify temperature input prompt for user clarity
    Updated temperature input prompt to be more flexible.

--- Day04/q_trans_input.py
+++ Day04/q_trans_input.py
@@ -5,7 +5,7 @@ from Calc_q_trans_library import calc_q_trans
 
 m = input("put the value of mass in kg unit: ")
 TC = input("Temperature checker, if in Degree Celsius, put 0, if in Fahrenheit, put 1, and if in Kelvin, put 3: ")
-T = input("put the value of temperature in K unit: ")
+T = input("put the value of temperature in your chosen unit: ")
 V = input("put the value of volume in m3 unit (if there is no V, put 1): ")
 
 q_trans, error = calc_q_trans(m, TC, T, V)
