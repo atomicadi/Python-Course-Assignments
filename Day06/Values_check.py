b27fef6b4bf4c1c7d4171ce1e2ac995ce0ce5594
Aditya Barman <147025377+atomicadi@users.noreply.github.com>
2026-05-24 17:40:36 +0300

    Add files via upload

---
+++ Day06/Values_check.py
@@ -0,0 +1,50 @@
+# Written and edited by Aditya Barman
+
+import pandas as pd
+
+df = pd.read_excel("enthalpy_dataset.xlsx")
+index_col = df.columns[0]
+stoicho_col = df.columns[1]
+B3LYP_col = df.columns[2]
+G4MP2_col = df.columns[3]
+G4_col = df.columns[4]
+CBS_QBS_col = df.column[5]
+
+
+df["diff_B3LYP_frm_G4MP2"] = df[B3LYP_col] - df[G4MP2_col]
+df["diff_B3LYP_frm_G4"] = df[B3LYP_col] - df[G4_col]
+df["diff_B3LYP_frm_CBS_QBS_col"] = df[B3LYP_col] - df[CBS_QBS_col]
+
+
+cols_to_keep = [
+    index_col, stoicho_col, B3LYP_col,
+    G4MP2_col, G4_col, CBS_QBS_col
+]
+
+# Conditions
+cond_G4MP2 = (
+    (df["diff_B3LYP_frm_G4MP2"].abs() < df["diff_B3LYP_frm_G4"].abs()) &
+    (df["diff_B3LYP_frm_G4MP2"].abs() < df["diff_B3LYP_frm_CBS_QBS"].abs())
+)
+
+cond_G4 = (
+    (df["diff_B3LYP_frm_G4"].abs() < df["diff_B3LYP_frm_G4MP2"].abs()) &
+    (df["diff_B3LYP_frm_G4"].abs() < df["diff_B3LYP_frm_CBS_QBS"].abs())
+)
+
+cond_CBS = (
+    (df["diff_B3LYP_frm_CBS_QBS"].abs() < df["diff_B3LYP_frm_G4MP2"].abs()) &
+    (df["diff_B3LYP_frm_CBS_QBS"].abs() < df["diff_B3LYP_frm_G4"].abs())
+)
+
+# Select rows
+output_G4MP2 = df.loc[cond_G4MP2, cols_to_keep]
+output_G4 = df.loc[cond_G4, cols_to_keep]
+output_CBS = df.loc[cond_CBS, cols_to_keep]
+
+# Save to Excel files
+output_G4MP2.to_excel("output_G4MP2.xlsx", index=False)
+output_G4.to_excel("output_G4.xlsx", index=False)
+output_CBS.to_excel("output_CBS_QBS.xlsx", index=False)
+
+print("Normal termination")
---
+++ Day06/txt2xl.py
@@ -0,0 +1,32 @@
+import re
+import pandas as pd
+
+rows = []
+
+with open("validation.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+
+        match = re.match(
+            r"^(\d{6})\s+(.+?)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)$",
+            line
+        )
+
+        if match:
+            rows.append(match.groups())
+
+df = pd.DataFrame(rows, columns=[
+    "Index",
+    "Stoichiometry",
+    "B3LYP_6-31G(2df,p)",
+    "G4MP2",
+    "G4",
+    "CBS-QB3"
+])
+
+for col in df.columns[2:]:
+    df[col] = pd.to_numeric(df[col])
+
+df.to_excel("enthalpy_dataset.xlsx", index=False)
+
+print(df.head())
